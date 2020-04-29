65567ebc614a5dbfd7c062436c3792d8afbea5f6
Joe Foley <joefoley@MacBook-Pro.localdomain>
2020-04-29 00:14:02 -0400

    folered out gdp/pp and plit it in 2 , before and after 1978.  Then did line graph of each

--- regress_discon.R
+++ regress_discon.R
@@ -7,3 +7,42 @@ library(tidyverse)
 library(ggplot2)
 library(lubridate)
 
+'''
+simple version discontinuou regression.
+devide the data at before 1978 and 1978 and later.
+get slope of each growth rate
+
+'''
+data<- read_csv("skinny_data.csv")
+
+income_per_capita<- data%>%
+        filter(variable == 'Adjusted net national income per capita (current US$)')
+
+income_per_capita$year <- as.Date(as.character(income_per_capita$year) , "%Y")
+
+income_per_capita$value <- as.numeric(income_per_capita$value)
+
+str(income_per_capita)
+
+Before_1978<- income_per_capita %>%
+            filter(year <  as.Date("1978-04-28"))
+
+After_1978<- income_per_capita %>%
+            filter(year >= as.Date("1978-04-28"))
+After_1978
+
+
+###################  Graphing ##########################
+Before<- ggplot(data = Before_1978, aes(x=year, y= value)) +
+  geom_line()+
+  ggtitle("Pre 1978 Per Capita Income")
+Before
+
+
+After<- ggplot(data = After_1978, aes(x=year, y= value)) +
+  geom_line()+
+  ggtitle("Post 1978 Per Capita Income")
+After
+
+
+
